fbe10e8e0d39115c9c76fb2d8fe26db5818e052d
Daniel Ndayegamiye <d.ndayegamiye@eagles.oc.edu>
2025-07-25 10:23:05 -0500

    Requests can now be auto-approved

--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, computed } from "vue";
+const emit = defineEmits(['createRequest', 'cancel'])
 
 const selectedSemId = ref(null);
 const props = defineProps({
   semesters: Array,
+  isReqWithinOneYear: Boolean
 });
+
+const showModal = ref(false);
 
 const filteredSemesters = computed(() => {
   const currentDate = new Date();
@@ -22,6 +26,22 @@ const filteredSemesters = computed(() => {
       return yearA - yearB;
     });
 });
+const handleSubmit = () => {
+
+  if (props.isReqWithinOneYear) {
+    showModal.value = true;
+  } else {
+    // Proceed with normal submit
+    emit('createRequest', selectedSemId.value);
+  }
+
+}
+
+const selectApprovalType = (approvalType) => {
+  showModal.value = false;
+  // Emit the createRequest event with the selected approval type
+  emit('createRequest', selectedSemId.value, approvalType);
+}
 </script>
 
 <template>
@@ -33,20 +53,9 @@ const filteredSemesters = computed(() => {
       style="padding-bottom: 16px; padding-left: 16px"
     >
       <v-card-title>Select The Semester</v-card-title>
-      <v-combobox
-        v-model="selectedSemId"
-        label="Semester"
-        :items="filteredSemesters"
-        item-title="semester"
-        return-object
-        variant="underlined"
-        style="float: left; width: 60%; padding-left: 16px"
-      ></v-combobox>
-      <v-btn
-        @click="$emit('createRequest', selectedSemId)"
-        rounded="lg"
-        elevation="2"
-        style="
+      <v-combobox v-model="selectedSemId" label="Semester" :items="filteredSemesters" item-title="semester"
+        return-object variant="underlined" style="float: left; width: 60%; padding-left: 16px"></v-combobox>
+      <v-btn @click="handleSubmit" rounded="lg" elevation="2" style="
           background-color: #118acb;
           color: white;
           float: right;
@@ -70,5 +79,35 @@ const filteredSemesters = computed(() => {
         Cancel
       </v-btn>
     </v-card>
+
+
+    <!-- Conditional Modal -->
+    <v-dialog v-model="showModal" max-width="400">
+      <v-card>
+        <v-card-title>Approval Type</v-card-title>
+        <v-card-text>
+          Do you want automatic or manual approval?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="selectApprovalType('auto')" rounded="lg" elevation="2" style="
+          background-color: #118acb;
+          color: white;
+          float: right;
+          margin-right: 24px;
+        ">
+            Automatic
+          </v-btn>
+          <v-btn @click="selectApprovalType('manual')" rounded="lg" elevation="2" style="
+          background-color: #118acb;
+          color: white;
+          float: right;
+          margin-right: 24px;
+        ">
+            Manual
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
